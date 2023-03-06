10bf1d00576feff7ccfa3943759212b27893d5f8
Sirius Tam <sirius731@gmail.com>
2023-03-06 16:05:18 +0800

    - Added Mock up for SSO API Server - Added Simulation on missing variable - Added README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+## Mock Up Portal
+
+
+### Port: 8007
+
+### Web 
+
+[/sso/login?key=[anything]&successUrl=[successUrl]&failureUrl=[failureUrl]
+](./screenshots/sso_portal.png)
+
+
+### API
+
+[/public/v1/verifyAccessToken](./screenshots/verify_token.png)
+
+[/public/v1/renewAccessToken](./screenshots/renew_access_token.png)
+
+[/public/v1/logout](./screenshots/logout.png)
+
+
+
+
+
+
--- src/routes/api.js
+++ src/routes/api.js
@@ -4,9 +4,25 @@ const jwt = require('jsonwebtoken')
 
 // Mock for verify Access Token
 router.post('/verifyAccessToken', (req, res, next) => {
+
+    // Check API KEY is provided
+    if(!req.headers.authorization) {
+        return res.status(200).json({ code: 30000, error: "Invalid key"})
+    }
+
     const token = req.body.token
+    if(!token){
+        return res.status(200).json({
+            code: 400,
+            payload: null,
+            error: "Bad request. \"token\" is not allowed to be empty"
+        })
+    }
+
     // Decode token to user data
     var content = jwt.verify(token, process.env.TOKEN_SECRET)
+    delete content["iat"]
+    delete content["exp"]
 
     // Add data to payload to return json result
     var payload = {
@@ -19,7 +35,21 @@ router.post('/verifyAccessToken', (req, res, next) => {
 
 // Mock for renew access token by refreshToken
 router.post('/renewAccessToken', (req, res, next) => {
+
+    // Check API KEY is provided
+    if(!req.headers.authorization) {
+        return res.status(200).json({ code: 30000, error: "Invalid key"})
+    }
+
     const refreshToken = req.body.refreshToken
+    if(!refreshToken){
+        return res.status(200).json({
+            code: 400,
+            payload: null,
+            error: "Bad request. \"refreshToken\" is not allowed to be empty"
+        })
+    }
+
     // Get User data from refresh Token
     var content = jwt.verify(refreshToken, process.env.TOKEN_SECRET)
     // Create Access token
@@ -39,7 +69,27 @@ router.post('/renewAccessToken', (req, res, next) => {
 
 // Mock for logout user by tokenList
 router.post('/logout', (req, res, next) => {
+
+    // Check API KEY is provided
+    if(!req.headers.authorization) {
+        return res.status(200).json({ code: 30000, error: "Invalid key"})
+    }
+
     const tokenList = req.body.tokenList
+    if(!tokenList){
+        return res.status(200).json({
+            code: 400,
+            payload: null,
+            error: "Bad request. \"tokenList\" is not allowed to be empty"
+        })
+    }
+    if(tokenList.length <=0){
+        return res.status(200).json({
+            code: 400,
+            payload: null,
+            error: "Bad request. \"tokenList\" must contain at least 1 items"
+        })
+    }
 
     // Generate a list of result based on the tokenList
     var resultList = tokenList.map(token => {
